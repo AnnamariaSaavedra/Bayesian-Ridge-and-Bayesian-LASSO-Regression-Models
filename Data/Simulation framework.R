d4665b3a28ff07c34f425f5c78cba142a8e5f320
AnnamariaSaavedra <164889752+AnnamariaSaavedra@users.noreply.github.com>
2026-01-14 11:13:30 -0500

    Update Simulation framework.R

--- Data/Simulation framework.R
+++ Data/Simulation framework.R
@@ -14,19 +14,20 @@ p_0 <- c(rep(5, 24)) # Number of non-zero elements
 
 Scenery <- data.frame(Escenario = 1:length(n), n = n, K = K, p = p, s = p_0)
 
-sigma2_sim <- list(sigma2_k2 = c(1, 2), sigma2_k3 = c(1, 2, 1), sigma2_k4 = c(1, 2, 1, 4)) # Variance parameter
+sigma2_sim <- list(sigma2_k2 = c(1, 2), sigma2_k3 = c(1, 2, 1), sigma2_k4 = c(1, 2, 1, 3)) # Variance parameter
 
-beta_sim <- matrix(data = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                            1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
-                   nrow = max(K), ncol = max(p), byrow = TRUE) # Mean parameter
+beta_sim <- matrix(data = c(1.5, 2.5, 3.5, 4.5, 5.5,
+                            -1, -2, -3, -4, -5,
+                            6, 5, 4, 3, 2,
+                            -6.5, -5.5, -4.5, -3.5, -2.5),
+                   nrow = max(K), ncol = 5, byrow = TRUE) # Mean parameter
 
 # 2. Data simulation
 
 # Auxiliary functions
 
 # Set the mean vector for the i-th scenery
+
 mean_parameter <- function(beta, beta_sim, K, p, p_0){
   if (K == 2) {
     for (i in 1:2) {
@@ -59,6 +60,7 @@ mean_parameter <- function(beta, beta_sim, K, p, p_0){
 }
 
 # Set the variance parameter for the i-th scenery
+
 variance_parameter <- function(sigma2_sim, K){
   if (K == 2) {
     sigma2 <- sigma2_sim[[1]]
@@ -69,6 +71,207 @@ variance_parameter <- function(sigma2_sim, K){
   }
   return(sigma2)
 }
+
+# Compute Deviance Information Criterion
+
+compute_DIC <- function(model, y, x, beta, sigma2){
+  LL_HAT <- sum(dnorm(x = y, mean = x%*%beta, sd = sqrt(sigma2), log = TRUE))
+  LL_B <- model$LL
+  
+  pDIC <- 2*(LL_HAT - mean(LL_B))
+  DIC <- -2*LL_HAT + 2*pDIC
+  
+  return(DIC)
+}
+
+# Compute Watanabe-Akaike Information Criterion
+
+compute_WAIC <- function(model, y, x, n){
+  LPPD <- 0
+  pWAIC <- 0
+  
+  for (i in 1:n) {
+    # LPPD
+    TMP <- dnorm(x = y[i], mean = x[i,]%*%t(model$BETA), sd = sqrt(model$SIGMA))
+    LPPD <- LPPD + log(mean(TMP))
+    # pWAIC
+    TMP_2 <- dnorm(x = y[i], mean = x[i,]%*%t(model$BETA), sd = sqrt(model$SIGMA), log = TRUE)
+    pWAIC <- pWAIC + 2*(log(mean(TMP)) - mean(TMP_2))
+  }
+  WAIC <- -2*LPPD + 2*pWAIC
+  
+  return(WAIC)
+}
+
+# Bayesian inference in finite mixture models
+
+function(model, B, K, p){
+  permu <- gtools::permutations(n = K, r = K) # Permutations
+  
+  # Average over the permuted spaces
+  # Objects where the samples of beta, and sigma2 will be stored
+  BETA_corrected <- vector("list", p)
+  SIGMA2_corrected <- vector("list")
+  
+  # Posterior distribution of K
+  k <- apply(X = model$XI, MARGIN = 1, function(x) length(unique(x)))
+  k_tab <- table(factor(x = k, levels = k, labels = k))
+  
+  beta_pos <- matrix(0, nrow = K, ncol = p)
+  sigma2_pos <- 0
+  
+  for (b in 1:B) {
+    if (length(unique(model$XI[b,])) == K) {
+      beta_pos <- beta_pos +
+        model$BETA[[b]][sort(unique(model$XI[b,])), , drop = FALSE] / K_tab[1] # Posterior mean
+      
+      sigma_pos <- sigma_pos + model$SIGMA[[b]] / K_tab[1] # Posterior mean
+    }
+  }
+  
+  for (b in 1:B) {
+    if (length(table(model$XI[b,])) == K) {
+      for (j in 1:p) {
+        beta_current <- model$BETA[[b]][sort(unique(model$XI[b,])), , drop = FALSE][,j]
+        # Reorder according to the permutations, and compute the distance of each sample to its posterior mean
+        dist <- apply(X = permu, 
+                      MARGIN = 1, 
+                      FUN = function(p) {
+                        permuted_beta <- beta_current[p]
+                        sum((permuted_beta - beta_pos[,j])^2)
+                      }
+        )
+        # Select the optimum permutation
+        best_permu <- permu[which.min(dist),]
+        BETA_corrected[[j]] <- rbind(BETA_corrected[[j]], beta_current[best_permu]) 
+      }
+      sigma2_current <- model$SIGMA[[b]]
+      # Reorder according to the permutations, and compute the distance of each sample to its posterior mean
+      dist <- apply(X = permu, 
+                    MARGIN = 1, 
+                    FUN = function(p) {
+                      permuted_sigma2 <- sigma2_current[p]
+                      sum((permuted_sigma2 - sigma2_pos)^2)
+                    }
+      )
+      # Select the optimum permutation
+      best_permu <- permu[which.min(dist),]
+      SIGMA2_corrected <- rbind(SIGMA2_corrected, sigma2_current[best_permu])
+    }
+  }
+  # Posterior mean, and 95% credible interval
+  beta <- matrix(data = NA, nrow = (p + 1), ncol = 3*K)
+  if (K == 2) {
+    for (i in 1:p) {
+      beta[i,1] <- colMeans(BETA_corrected[[i]])[1]
+      beta[i,2] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[1,1]
+      beta[i,3] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[2,1]
+      beta[i,4] <- colMeans(BETA_corrected[[i]])[2]
+      beta[i,5] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[1,2]
+      beta[i,6] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[2,2]
+    }
+    beta[(p + 1),1] <- mean(SIGMA2_corrected[[1]])
+    beta[(p + 1),2] <- quantile(SIGMA2_corrected[[1]], probs = c(0.025, 0.975))[1]
+    beta[(p + 1),3] <- quantile(SIGMA2_corrected[[1]], probs = c(0.025, 0.975))[2]
+    beta[(p + 1),4] <- mean(SIGMA2_corrected[[2]])
+    beta[(p + 1),5] <- quantile(SIGMA2_corrected[[2]], probs = c(0.025, 0.975))[1]
+    beta[(p + 1),6] <- quantile(SIGMA2_corrected[[2]], probs = c(0.025, 0.975))[2]  
+  } else if (K == 3){
+    for (i in 1:p) {
+      beta[i,1] <- colMeans(BETA_corrected[[i]])[1]
+      beta[i,2] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[1,1]
+      beta[i,3] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[2,1]
+      beta[i,4] <- colMeans(BETA_corrected[[i]])[2]
+      beta[i,5] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[1,2]
+      beta[i,6] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[2,2]
+      beta[i,7] <- colMeans(BETA_corrected[[i]])[3]
+      beta[i,8] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[1,3]
+      beta[i,9] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[2,3]
+    }
+    beta[(p + 1),1] <- mean(SIGMA2_corrected[[1]])
+    beta[(p + 1),2] <- quantile(SIGMA2_corrected[[1]], probs = c(0.025, 0.975))[1]
+    beta[(p + 1),3] <- quantile(SIGMA2_corrected[[1]], probs = c(0.025, 0.975))[2]
+    beta[(p + 1),4] <- mean(SIGMA2_corrected[[2]])
+    beta[(p + 1),5] <- quantile(SIGMA2_corrected[[2]], probs = c(0.025, 0.975))[1]
+    beta[(p + 1),6] <- quantile(SIGMA2_corrected[[2]], probs = c(0.025, 0.975))[2]
+    beta[(p + 1),7] <- mean(SIGMA2_corrected[[3]])
+    beta[(p + 1),8] <- quantile(SIGMA2_corrected[[3]], probs = c(0.025, 0.975))[1]
+    beta[(p + 1),9] <- quantile(SIGMA2_corrected[[3]], probs = c(0.025, 0.975))[2]
+  } else{
+    for (i in 1:p) {
+      beta[i,1] <- colMeans(BETA_corrected[[i]])[1]
+      beta[i,2] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[1,1]
+      beta[i,3] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[2,1]
+      beta[i,4] <- colMeans(BETA_corrected[[i]])[2]
+      beta[i,5] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[1,2]
+      beta[i,6] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[2,2]
+      beta[i,7] <- colMeans(BETA_corrected[[i]])[3]
+      beta[i,8] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[1,3]
+      beta[i,9] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[2,3]
+      beta[i,10] <- colMeans(BETA_corrected[[i]])[4]
+      beta[i,11] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[1,4]
+      beta[i,12] <- apply(BETA_corrected[[i]], MARGIN = 2, FUN = quantile, probs = c(0.025, 0.975))[2,4]
+    }
+    beta[(p + 1),1] <- mean(SIGMA2_corrected[[1]])
+    beta[(p + 1),2] <- quantile(SIGMA2_corrected[[1]], probs = c(0.025, 0.975))[1]
+    beta[(p + 1),3] <- quantile(SIGMA2_corrected[[1]], probs = c(0.025, 0.975))[2]
+    beta[(p + 1),4] <- mean(SIGMA2_corrected[[2]])
+    beta[(p + 1),5] <- quantile(SIGMA2_corrected[[2]], probs = c(0.025, 0.975))[1]
+    beta[(p + 1),6] <- quantile(SIGMA2_corrected[[2]], probs = c(0.025, 0.975))[2]
+    beta[(p + 1),7] <- mean(SIGMA2_corrected[[3]])
+    beta[(p + 1),8] <- quantile(SIGMA2_corrected[[3]], probs = c(0.025, 0.975))[1]
+    beta[(p + 1),9] <- quantile(SIGMA2_corrected[[3]], probs = c(0.025, 0.975))[2]
+    beta[(p + 1),10] <- mean(SIGMA2_corrected[[3]])
+    beta[(p + 1),11] <- quantile(SIGMA2_corrected[[3]], probs = c(0.025, 0.975))[1]
+    beta[(p + 1),12] <- quantile(SIGMA2_corrected[[3]], probs = c(0.025, 0.975))[2]
+  }
+}
+
+# Deviance Information Criterion
+
+beta <- beta[,c(1,4)] # Estimacion puntual corregida
+
+LL_HAT <- NA
+
+for (i in 1:n) {
+  LL_HAT[i] <- dnorm(x = y[i], mean = x[i,]%*%beta[,data_2$xi[i]], sd = sqrt(sigma2[data_2$xi[i]]), log = TRUE) 
+}
+
+LL_B <- model$LL
+
+pDIC <- 2*(sum(LL_HAT) - mean(LL_B))
+
+DIC <- -2*sum(LL_HAT) + 2*pDIC
+
+# Watanabe-Akaike Information Criterion
+
+B <- 10000; n <- length(data_1$y)
+
+TMP <- matrix(data = NA, nrow = B, ncol = n)
+
+TMP_2 <- matrix(data = NA, nrow = B, ncol = n)
+
+for (i in 1:n) {
+  for (b in 1:B) {
+    xi <- model$XI
+    beta <- model$BETA[[b]][xi[b,i], , drop = FALSE]
+    sigma2 <- model$SIGMA[[b]][xi[b,i]]
+    
+    # LPPD
+    TMP[b,i] <- dnorm(x = y[i], mean = x[i,]%*%t(beta), sd = sqrt(sigma2))
+    
+    #pWAIC
+    TMP_2[b,i] <- dnorm(x = y[i], mean = x[i,]%*%t(beta), sd = sqrt(sigma2), log = TRUE) 
+  }
+}
+
+LPPD <- sum(log(colMeans(TMP)))
+
+pWAIC <- 2*sum((log(colMeans(TMP)) - colMeans(TMP_2)))
+
+WAIC <- -2*LPPD + 2*pWAIC
+
+# 3. Create dataset for each scenary
 
 create_dataset <- function(Scenary) {
   # Objects where the response variable, explanatory variables, density function, and cluster indicator variable will be stored
@@ -151,3 +354,8 @@ create_dataset <- function(Scenary) {
   }
   return(list(X = X, Y = Y, F_TRUE = F_TRUE, XI = XI, BETA = BETA, SIGMA2 = SIGMA2))
 }
+
+data <- create_dataset(Scenary)
+
+save(data, file = "~/Downloads/Dataset - Simulation framework")
+
