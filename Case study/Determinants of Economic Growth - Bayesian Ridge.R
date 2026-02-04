18fedad961f13a7fda4117daaf488eed8adccd14
AnnamariaSaavedra <164889752+AnnamariaSaavedra@users.noreply.github.com>
2026-02-04 10:08:43 -0500

    Update Determinants of Economic Growth - Bayesian Ridge.R

--- Case study/Determinants of Economic Growth - Bayesian Ridge.R
+++ Case study/Determinants of Economic Growth - Bayesian Ridge.R
@@ -146,7 +146,7 @@ LAMBDA_SD <- round(sd(M2$LAMBDA), 4) # Posterior standard deviation
 
 CI_LAMBDA <- round(quantile(x = M2$LAMBDA, probs = c(0.025, 0.975)), 4) # 95% credible interval
 
-# 6. Compute information criterion and k-fold cross validation
+# 6. Compute information criterion and cross validation
 
 # Deviance Information Criterion
 
@@ -175,52 +175,47 @@ for (i in 1:n) {
 
 WAIC <- -2*LPPD + 2*pWAIC
 
-# 2-fold cross validation
+# Cross validation
 
-cross_validation <- function(fold, y, x, p, a, c, d){
-  id <- kfold(x = y, k = fold)
+cross_validation <- function(y, x, n, p, a, c, d){
+  index <- sample(1:n, size = 0.7*n)
+  
+  y_train <- y[index]; x_train <- x[index,] # Train data set
+  y_test <- y[-index]; x_test <- x[-index,] # Test data set
   
   # Objects where the mean absolute error, and the mean squared prediction error will be stored
   mape <- NULL
   mspe <- NULL
   
-  for (j in 1:fold) {
-    y_train <- y[id != j]; x_train <- x[id != j,] # Train data set
-    y_test <- y[id = j]; x_test <- x[id = j] # Test data set
-    
-    n <- length(y_train)
-    
-    # Hyperparameter elicitation
-    beta_OLS <- solve(t(x_train)%*%x_train)%*%t(x_train)%*%y_train
-    residuals <- y_train - x_train%*%beta_OLS
-    sigma2_OLS <- sum(residuals^2)/(n - p)
-    
-    # Fit G prior regression model
-    M2 <- Gibbs_ridge(y_train, x_train, a, b = a*sigma2_OLS, c, d, n, p,
-                      n_sams = 10000, # Set the number of effective samples
-                      n_skip = 10, # Accounting for Markov chain autocorrelation will require systematic sampling
-                      n_burn = 1000) # Set the number of burn-in samples
-    
-    
-    # Posterior mean for beta
-    beta <- colMeans(M2$BETA)
-    
-    # Linear predictor
-    y_hat <- x_test%*%beta
-    
-    # Compute mean absolute prediction error
-    mape_ridge <- mean(abs(y_test - y_hat))
-    
-    # Compute mean squared prediction error
-    mspe_ridge <- mean((y_test - y_hat)^2)
-    
-    mape <- rbind(mape, mape_ridge)
-    mspe <- rbind(mspe, mspe_ridge)
-  }
-  return(list(mape = mean(mape), mspe = mean(mspe)))
+  n <- length(y_train)
+  
+  # Hyperparameter elicitation
+  beta_OLS <- solve(t(x_train)%*%x_train)%*%t(x_train)%*%y_train
+  residuals <- y_train - x_train%*%beta_OLS
+  sigma2_OLS <- sum(residuals^2)/(n - p)
+  
+  # Fit Bayesian Ridge regression model
+  M2 <- Gibbs_ridge(y_train, x_train, a, b = a*sigma2_0, c, d, n, p,
+                    n_skip = 10, # Accounting for Markov chain autocorrelation will require systematic sampling,
+                    n_sams = 10000, # Set the number of effective samples
+                    n_burn = 1000) # Set the number of burn-in samples
+  
+  # Posterior mean for beta
+  beta <- colMeans(M2$BETA)
+  
+  # Linear predictor
+  y_hat <- x_test%*%beta
+  
+  # Compute mean absolute prediction error
+  mape <- mean(abs(y_test - y_hat))
+  
+  # Compute mean squared prediction error
+  mspe <- mean((y_test - y_hat)^2)
+  
+  return(list(mape = mape, mspe = mspe))
 }
 
-cross_validation_M2 <- cross_validation(fold = 2, y, x, p, a, c, d)
+cross_validation_M2 <- cross_validation(y, x, n, p, a, c, d)
 
 # 7. Monte Carlo samples from the posterior predictive distribution of test statistics
 
