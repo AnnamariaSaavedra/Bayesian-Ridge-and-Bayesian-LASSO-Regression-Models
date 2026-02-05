e56761d3ab2d7e3b5eee2e40f877a17c414fb686
AnnamariaSaavedra <164889752+AnnamariaSaavedra@users.noreply.github.com>
2026-02-04 21:17:08 -0500

    Update Determinants of Economic Growth - Bayesian Ridge.R

--- Case study/Determinants of Economic Growth - Bayesian Ridge.R
+++ Case study/Determinants of Economic Growth - Bayesian Ridge.R
@@ -188,11 +188,17 @@ WAIC <- -2*LPPD + 2*pWAIC
 
 # Cross validation
 
-cross_validation <- function(y, x, n, p, a, c, d){
+cross_validation <- function(y, data, n, p, a, c, d){
+  x <- Data %>%
+    dplyr::select(-c(CODE, GR6096)) %>% # Set the matrix containing the explanatory variables
+    mutate(INT = 1) %>% # Create the intercept column
+    as.matrix()
+  
+  # Create the train and test dataset
   index <- sample(1:n, size = 0.7*n)
   
-  y_train <- y[index]; x_train <- x[index,] # Train data set
-  y_test <- y[-index]; x_test <- x[-index,] # Test data set
+  y_train <- y[index]; x_train <- x[index,] # Train dataset
+  y_test <- y[-index]; x_test <- x[-index,] # Test dataset
   
   # Objects where the mean absolute error, and the mean squared prediction error will be stored
   mape <- NULL
@@ -204,6 +210,15 @@ cross_validation <- function(y, x, n, p, a, c, d){
   beta_OLS <- solve(t(x_train)%*%x_train)%*%t(x_train)%*%y_train
   residuals <- y_train - x_train%*%beta_OLS
   sigma2_OLS <- sum(residuals^2)/(n - p)
+  
+  x <- Data %>%
+    dplyr::select(-c(CODE, GR6096)) %>% # Set the matrix containing the explanatory variables
+    scale(center = TRUE, scale = TRUE) %>% # Standardize the explanatory variables
+    as.matrix()
+  x <- cbind(x, 1) # Create the intercept column
+  
+  x_train <- x[index,] # Standardized train dataset
+  x_test <- x[-index,] # Standardized test dataset
   
   # Fit Bayesian Ridge regression model
   M2 <- Gibbs_ridge(y_train, x_train, a, b = a*sigma2_0, c, d, n, p,
@@ -226,7 +241,7 @@ cross_validation <- function(y, x, n, p, a, c, d){
   return(list(mape = mape, mspe = mspe))
 }
 
-cross_validation_M2 <- cross_validation(y, x, n, p, a, c, d)
+cross_validation_M2 <- cross_validation(y, Data, n, p, a, c, d)
 
 # 7. Monte Carlo samples from the posterior predictive distribution of test statistics
 
